b51bab4a3f7e3098a040a52efc2c7c3834344e4a
David Enke <david@enke.dev>
2023-03-13 15:56:51 +0100

    feat: added plugin for grouping

---
+++ src/cem-plugin-grouping/README.md
@@ -0,0 +1,6 @@
+# <img align="left" src="https://github.com/webcomponents-preview/client/raw/main/src/assets/icons/logo.svg" alt="WCP Logo" height="43px"> @webcomponents-preview/cem-plugin-grouping
+
+[![Workflow status](https://github.com/webcomponents-preview/cem-plugins/actions/workflows/checks.yml/badge.svg)](https://github.com/webcomponents-preview/cem-plugins/actions/workflows/checks.yml)
+[![npm Release](https://badgen.net/npm/v/@webcomponents-preview/cem-plugin-grouping/latest?label=@webcomponents-preview/cem-plugin-grouping&color=cyan&icon=npm)](https://www.npmjs.com/package/@webcomponents-preview/cem-plugin-grouping)
+
+Allows grouping of custom elements in the preview client by providing a group name for a custom groups property.
---
+++ src/cem-plugin-grouping/index.ts
@@ -0,0 +1,28 @@
+import type { Plugin } from '@custom-elements-manifest/analyzer';
+import { findDeclaration } from '../utils/plugin.utils';
+
+// as the plugin is create by factory function, we can provide some options
+type PluginOptions = {
+  /**
+   * Provide a function that delivers the groups for the given component path.
+   */
+  addGroups(componentPath?: string): string[];
+};
+
+/**
+ * A plugin for `@custom-elements-manifest/analyzer` that can be used to provide an additional `groups` property
+ * to a module's documentation. This can then be used by the WCP to group the navigation accordingly.
+ */
+export function customElementGroupingPlugin({ addGroups }: PluginOptions): Plugin {
+  return {
+    name: 'custom-element-grouping',
+    analyzePhase({ ts, node, moduleDoc }) {
+      // we just look up docs for classes
+      if (ts.isClassDeclaration(node)) {
+        const doc = findDeclaration<CemJSDoc & { groups: string[] }>(moduleDoc, node.name?.getText());
+        if (!doc) return;
+        doc.groups = addGroups(moduleDoc.path) ?? [];
+      }
+    },
+  };
+}
