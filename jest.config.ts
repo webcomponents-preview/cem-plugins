6687a8f9274a5d4234acd063692f16f7ec9b7093
David Enke <david@enke.dev>
2023-03-17 00:53:13 +0100

    chore: updated test deps
    and made it work, added simple button fixture

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,13 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 // Sync object
 const config: JestConfigWithTsJest = {
-  globals: {
-    configuration: {},
-    Liferay: {},
-  },
-  preset: 'ts-jest/presets/js-with-ts',
-  testMatch: ['<rootDir>/src/**/*(*.)@(spec|test).[tj]s?(x)'],
+  extensionsToTreatAsEsm: ['.ts'],
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/src/**/*(*.)@(spec|test).[tj]s'],
   testPathIgnorePatterns: ['/node_modules/'],
-  transform: { '^.+\\.m?[tj]s$': 'ts-jest' },
+  transform: { '^.+\\.m?[tj]sx?$': ['ts-jest', { useESM: true }] },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(@custom-elements-manifest|@github|cem-plugins))'],
   moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'd.ts', 'ts', 'tsx', 'json', 'node'],
 };
 
